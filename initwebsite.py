440ec21c3977413ea81fd09479becb6969022790
parakosm <161525773+parakosm@users.noreply.github.com>
2026-04-01 10:48:01 +1300

    Sign Up Now Uses Database

--- initwebsite.py
+++ initwebsite.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -10,14 +11,20 @@ def index():
 def index_signinsubmitted():
     if request.method == 'POST':
         form_data = request.form
-        print(f"Sign up: {dict(form_data)}") 
+        print(f"Sign in attempt: {dict(form_data)}") 
         return render_template('indexwsignin.html.jinja', username=form_data['username'])
     
 @app.route('/submit-signup', methods=['POST'])
 def index_signupsubmitted():
     if request.method == 'POST':
         form_data = request.form
-        print(f"Sign up: {dict(form_data)}") 
+        print(f"Sign up attempt: {dict(form_data)}") 
+        database = sqlite3.connect("Database\database.db")
+        cursor = database.cursor()
+        dob = f"{form_data['dd']}-{form_data['mm']}-{form_data['yyyy']}"
+        cursor.execute("INSERT INTO Users (username, email, password, DOB) VALUES (?, ?, ?, ?)", (form_data['username'], form_data['email'], form_data['password'], dob))
+        database.commit()
+        database.close()
         return render_template('indexwsignup.html.jinja', username=form_data['username'])
 
 if __name__ == '__main__':
