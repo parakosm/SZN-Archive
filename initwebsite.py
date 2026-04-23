0bab43afc11860fbca501aeb5545156770c2751a
parakosm <161525773+parakosm@users.noreply.github.com>
2026-04-23 14:04:31 +1200

    Finished Implementing Cookies

--- initwebsite.py
+++ initwebsite.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html.jinja')
+    user = request.cookies.get('User')
+    if user is None:
+        user = "None"
+    return render_template('index.html.jinja', user=user)
 
 @app.route('/submit-signin', methods=['POST'])
 def index_signinsubmitted():
@@ -19,9 +22,9 @@ def index_signinsubmitted():
         database.close()
         try:
             if user[0] == form_data['password']:
-                signin = make_response("Sign In Successful!")
-                signin.setcookie("User", form_data['username'])
-                return render_template('indexwsignin.html.jinja', username=form_data['username'])
+                signin = make_response(render_template('indexwsignin.html.jinja', username=form_data['username']))
+                signin.set_cookie("User", form_data['username'])
+                return signin
             else:
                 return render_template('indexwsigninfail.html.jinja')
         except:
@@ -39,7 +42,9 @@ def index_signupsubmitted():
         cursor.execute("INSERT INTO Users (username, email, password, DOB) VALUES (?, ?, ?, ?)", (form_data['username'], form_data['email'], form_data['password'], dob))
         database.commit()
         database.close()
-        return render_template('indexwsignup.html.jinja', username=form_data['username'])
+        signup = make_response(render_template('indexwsignup.html.jinja', username=form_data['username']))
+        signup.set_cookie("User", form_data['username'])
+        return signup
 
 if __name__ == '__main__':
     app.run(debug=True)
