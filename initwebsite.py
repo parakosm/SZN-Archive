03227072e68fa7321cc0c2c7ba3735db678a0d1b
parakosm <161525773+parakosm@users.noreply.github.com>
2026-05-01 10:49:45 +1200

    Finished Theoretical Algorithms for Browse

--- initwebsite.py
+++ initwebsite.py
@@ -68,7 +68,7 @@ def upload_songsubmitted():
         streaminglink = form_data['streaminglink']
         forfansof = form_data['FFO']
         user = request.cookies.get('User')
-        cursor.execute(f"SELECT songid FROM Songs")
+        cursor.execute("SELECT songid FROM Songs")
         ids_available = cursor.fetchall()
         ids_available = [row[0] for row in ids_available]
         newid = max(ids_available) + 1
@@ -76,5 +76,72 @@ def upload_songsubmitted():
         database.commit()
         database.close()
         return render_template('uploadsuccess.html.jinja', user=user)
+    
+@app.route('/browse')
+def browse():
+    user = request.cookies.get('User')
+    if user is None:
+        user = "None"
+    return render_template('browse.html.jinja', user=user)
+
+@app.route('/browse-unrated')
+def browseunrated():
+    user = request.cookies.get('User')
+    if user is None:
+        user = "None"
+    database = sqlite3.connect(dbpath)
+    cursor = database.cursor()
+    cursor.execute("SELECT songid FROM Songs")
+    allsongids = cursor.fetchall()
+    allsongids = [row[0] for row in allsongids]
+    cursor.execute("SELECT songid FROM Ratnings")
+    ratedsongids = cursor.fetchall()
+    ratedsongids = [row[0] for row in ratedsongids]
+    unratedsongids = list(set(allsongids) ^ set(ratedsongids))
+    song = unratedsongids.choice()
+    cursor.execute(f"SELECT title FROM Songs WHERE songid = \'{song}\'")
+    title = cursor.fetchone()
+    cursor.execute(f"SELECT release FROM Songs WHERE songid = \'{song}\'")
+    release = cursor.fetchone()
+    cursor.execute(f"SELECT streaminglink FROM Songs WHERE songid = \'{song}\'")
+    streaminglink = cursor.fetchone()
+    cursor.execute(f"SELECT FFO FROM Songs WHERE songid = \'{song}\'")
+    forfansof = cursor.fetchone()
+    database.close()
+    return render_template('rate.html.jinja', user=user, title=title, release=release, streaminglink=streaminglink, forfansof=forfansof)
+
+@app.route('/browse-rated')
+def browserated():
+    if request.method == 'POST':
+        user = request.cookies.get('User')
+        if user is None:
+            user = "None"
+
+        form_data = request.form
+        database = sqlite3.connect(dbpath)
+        cursor = database.cursor()
+        cursor.execute("SELECT songid FROM Songs")
+        allsongids = cursor.fetchall()
+        allsongids = [row[0] for row in allsongids]
+        songfound = False
+        while songfound == False:
+            song = allsongids.choice()
+            cursor.execute(f"SELECT rating FROM Ratings WHERE songid = \'{song}\'")
+            ratings = cursor.fetchall()
+            ratings = [row[0] for row in ratings]
+            rating = sum(rating) / len(rating)
+            if rating >= form_data["minimum"] and rating <= form_data["maximum"]:
+                songfound = True
+        cursor.execute(f"SELECT title FROM Songs WHERE songid = \'{song}\'")
+        title = cursor.fetchone()
+        cursor.execute(f"SELECT release FROM Songs WHERE songid = \'{song}\'")
+        release = cursor.fetchone()
+        cursor.execute(f"SELECT streaminglink FROM Songs WHERE songid = \'{song}\'")
+        streaminglink = cursor.fetchone()
+        cursor.execute(f"SELECT FFO FROM Songs WHERE songid = \'{song}\'")
+        forfansof = cursor.fetchone()
+        database.close()
+        return render_template('rate.html.jinja', user=user, title=title, release=release, streaminglink=streaminglink, forfansof=forfansof, rating=rating)
+
 if __name__ == '__main__':
     app.run(debug=True)
